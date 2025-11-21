c658787be85940ceb57fdd52a1cc57e6fa72778b
Atira <atiray@example.com>
2025-11-21 15:48:14 +0700

    ada

--- core/Database.php
+++ core/Database.php
@@ -11,13 +11,13 @@ class Database
     public $conn;
 
     public function __construct() {
-        $this->type = getenv('DB_TYPE') ?: 'mysql'; // vercel pakai pgsql
+        $this->type = getenv('DB_TYPE') ?: 'pgsql'; // vercel pakai pgsql
         $this->host = getenv('DB_HOST') ?: 'localhost';
-        $this->port = getenv('DB_PORT') ?: '3306';
-        $this->db_name = getenv('DB_NAME') ?: 'kampus_db';
-        $this->username = getenv('DB_USER') ?: 'root';
+        $this->port = getenv('DB_PORT') ?: '5432';
+        $this->db_name = getenv('DB_NAME') ?: 'neondb';
+        $this->username = getenv('DB_USER') ?: 'neondb_owner';
         $this->password = getenv('DB_PASS') ?: '';
-        $this->sslmode = getenv('DB_SSLMODE') ?: ''; // vercel pakai require
+        $this->sslmode = getenv('DB_SSLMODE') ?: 'require'; // vercel pakai require
         // getenv() hanya akan mengambil nilai dari environment variable sistem, bukan dari file .env apa pun.
         // getenv() hanya dipakai untuk production server
     }
